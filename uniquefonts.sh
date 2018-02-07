0a3cabff97cb8dfa84b7ed3c51f3471d582f28e7
Brendan <brendan.leason.4@gmail.com>
2018-02-07 13:19:45 -0700

    Update uniquefonts.sh

--- uniquefonts.sh
+++ uniquefonts.sh
@@ -24,27 +24,32 @@ else
 fi
 for file1 in "${locDir}"/*; do
     file1_base="$(basename "$file1")"
-    if [ -d "$file1" ]; then
+    [ "${file1_base}" = 'fonts.scale' ] || [ "${file1_base}" = 'fonts.scale' ] && continue
+    if [ -d "${file1}" ]; then
         echo "File 1 ->${file1} is a dir!"
         for file2 in "${file1}"/*; do
-            file2_base="$(basename "$file2")"
+            file2_base="$(basename "${file2}")"
+            [ "${file2_base}" = 'fonts.scale' ] || [ "${file2_base}" = 'fonts.scale' ] && continue
             if [ -f "${sysDir}/${file1_base}/${file2_base}" ]; then
-                echo "${file1}/${file2_base}		is a duplicate, deleting"
-                ! $DEBUG && rm -f "$file2"
+                #echo "${file1}/${file2_base}		is a duplicate, deleting"
+                printf "%-80s\t%s\n" "${file1}/${file2_base}" "is a dublicate, deleting"
+                ! ${DEBUG} && rm -f "${file2}"
             fi
         done
-        
+
     else
         if [ -f "${sysDir}/${file1_base}" ]; then
-            echo "${file1}		is a duplicate, deleting"
-            ! $DEBUG && rm -f "$file1"
-            
+            #echo "${file1}		is a duplicate, deleting"
+            printf "%-80s\t%s\n" "${file1}" "is a dublicate, deleting"
+            ! ${DEBUG} && rm -f "${file1}"
+
         else
             for sysFile1 in "${sysDir}"/*; do
                 if [ -d "$sysFile1" ]; then
                     if [ -f "${sysFile1}/${file1_base}" ]; then
-                        echo "${file1}		is a duplicate, deleting"
-                        ! $DEBUG && rm -f "$file1"
+                        #echo "${file1}		is a duplicate, deleting"
+                        printf "%-80s\t%s\n" "${file1}" "is a dublicate, deleting"
+                        ! ${DEBUG} && rm -f "${file1}"
                     fi
                 fi
             done
